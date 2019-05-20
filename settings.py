fc1cad569e029c78a2af869226b8c8176dbbbfaf
BigKrokha <43364990+BigKrokha@users.noreply.github.com>
2019-05-21 00:29:18 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ GROUP_ID = 180454656
 MODULES = ["GiveMeMoneyFinal", "HelloMsg",
            "ByeMsg", "WhoIMsg", "WhoYouMsg", "MondayFirstSchedule",
            "MondaySecondSchedule", "TuesdayFirstSchedule",
-           "TuesdaySecondSchedule", "GetAllDocuments"]
+           "TuesdaySecondSchedule", "GetAllDocuments", "GetKUG", "GetKUGoch", "GetKUGzaoch", "GetKUGoch_zaoch"]
 
 DOCUMENTS = ["GetFile1", "GetFile2", "GetFile3", "GetFile4", "GetFile5", "GetFile6", "GetFile7", "GetFile8", "GetFile9",
              "GetFile10", "GetFile11", "GetFile12", "GetFile13", "GetFile14", "GetFile15", "GetFile16", "GetFile17",
