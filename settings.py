ae6669c143367be486fe498fe0cc49bfdcceea57
Vladislav <bolilyivs@gmail.com>
2019-05-21 10:06:36 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-GROUP_TOKEN = "c71283221733a6f323993735ad971575ff4fc92ded92812cb2fbf3159e79e5768d1fdef874348ac3a83fc"
+GROUP_TOKEN = ""
 GROUP_ID = 180454656
 MODULES = ["Help", "GiveMeMoney", "HelloMsg",
            "ByeMsg", "WhoIMsg", "WhoYouMsg", "GetAllDocuments", "GetKUG",
